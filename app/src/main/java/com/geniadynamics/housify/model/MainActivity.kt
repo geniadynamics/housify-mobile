d5398528a9f95962b6a9c399102ef6d597b6e851
db <diogo.bernardo.dev@gmail.com>
2023-10-29 17:44:59 +0000

    Change project structure

--- app/src/main/java/com/geniadynamics/housify/model/MainActivity.kt
+++ app/src/main/java/com/geniadynamics/housify/model/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.geniadynamics.housify
+package com.geniadynamics.housify.model
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.geniadynamics.housify.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
