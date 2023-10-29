b269b27d3d65750bbbdc3ab83e03fd52043d236e
João Ribeiro <jpmr0703@gmail.com>
2023-10-29 17:37:22 +0000

    add text box

--- app/src/main/java/com/geniadynamics/housify/MainActivity.kt
+++ app/src/main/java/com/geniadynamics/housify/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.geniadynamics.housify
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
+import com.geniadynamics.housify.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editText = findViewById<EditText>(R.id.editText)
+        val submitButton = findViewById<Button>(R.id.submitButton)
+
+        submitButton.setOnClickListener {
+            val userInput = editText.text.toString()
+            if (userInput.isNotEmpty()) {
+                Toast.makeText(this, "Texto escrito: $userInput", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Escreva algo antes de enviar", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
