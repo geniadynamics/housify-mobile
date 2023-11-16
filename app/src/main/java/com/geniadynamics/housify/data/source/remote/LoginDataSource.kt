576628cec35edd6750bdb62630b1a15800b117d1
Diogo Bernardo <db@Diogos-MacBook-Pro.local>
2023-11-16 13:57:39 +0000

    Add logo xml files, edit login UI

--- app/src/main/java/com/geniadynamics/housify/data/source/remote/LoginDataSource.kt
+++ app/src/main/java/com/geniadynamics/housify/data/source/remote/LoginDataSource.kt
@@ -11,12 +11,12 @@ import java.util.UUID
 class LoginDataSource {
 
     fun login(username: String, password: String): Result<LoggedInUser> {
-        try {
+        return try {
             // TODO: handle loggedInUser authentication
             val fakeUser = LoggedInUser(UUID.randomUUID().toString(), "Jane Doe")
-            return Result.Success(fakeUser)
+            Result.Success(fakeUser)
         } catch (e: Throwable) {
-            return Result.Error(IOException("Error logging in", e))
+            Result.Error(IOException("Error logging in", e))
         }
     }
 
