4e6c116ca2cacb5b44f29a8083b7aeaa808ed3f0
Diogo Bernardo <diogo.bernardo.dev@gmail.com>
2023-12-19 23:53:10 +0000

    add Welcome activity

--- app/src/main/kotlin/org/geniadynamics/housify/ui/main/MainActivity.kt
+++ app/src/main/kotlin/org/geniadynamics/housify/ui/main/MainActivity.kt
@@ -7,12 +7,29 @@ import org.geniadynamics.housify.R
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
+import org.geniadynamics.housify.ui.home.HomeActivity
 import org.geniadynamics.housify.ui.login.LoginActivity
+import org.geniadynamics.housify.ui.welcome.WelcomeActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        // Assuming you have a function to check JWT token validity
+        if (hasValidJwtToken()) {
+            // If token is valid, go to home page
+            startActivity(Intent(this, HomeActivity::class.java))
+        } else {
+            // If no valid token, go to welcome page
+            startActivity(Intent(this, WelcomeActivity::class.java))
+        }
+        // Close MainActivity
+        finish()
+    }
+
+    private fun hasValidJwtToken(): Boolean {
+        // Implement your logic to check for JWT tokens
+        // This is just a placeholder for your actual token validation logic
+        return false
     }
 }
--- app/src/main/kotlin/org/geniadynamics/housify/ui/welcome/WelcomeActivity.kt
+++ app/src/main/kotlin/org/geniadynamics/housify/ui/welcome/WelcomeActivity.kt
@@ -26,5 +26,4 @@ class WelcomeActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
-
 }
