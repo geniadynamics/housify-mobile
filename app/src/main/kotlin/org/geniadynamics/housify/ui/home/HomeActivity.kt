4a2a2dac3addad5836c2ef08368753e3a698e46a
Diogo Bernardo <diogo.bernardo.dev@gmail.com>
2024-01-05 18:50:23 +0000

    add camera Activity

--- app/src/main/kotlin/org/geniadynamics/housify/ui/home/HomeActivity.kt
+++ app/src/main/kotlin/org/geniadynamics/housify/ui/home/HomeActivity.kt
@@ -1,14 +1,23 @@
 package org.geniadynamics.housify.ui.home
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import org.geniadynamics.housify.R
+import org.geniadynamics.housify.ui.camera.CameraActivity
 
 class HomeActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnOpenCamera = findViewById<Button>(R.id.btnOpenCamera)
+        btnOpenCamera.setOnClickListener {
+            val intent = Intent(this, CameraActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
