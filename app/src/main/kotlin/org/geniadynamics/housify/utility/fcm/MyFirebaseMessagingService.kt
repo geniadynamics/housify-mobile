6449a0926a2d37c108d7f03bb276037dbbb49c4a
Edgar Baptista <91727653+Edgarfsb@users.noreply.github.com>
2024-01-10 20:41:27 +0000

    Firebase notification completion

--- app/src/main/kotlin/org/geniadynamics/housify/utility/fcm/MyFirebaseMessagingService.kt
+++ app/src/main/kotlin/org/geniadynamics/housify/utility/fcm/MyFirebaseMessagingService.kt
@@ -1,8 +1,59 @@
 package org.geniadynamics.housify.utility.fcm
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import org.geniadynamics.housify.ui.main.MainActivity
+import org.geniadynamics.housify.R
 
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
+    val TAG = "MAIN"
 
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "Refreshed token: $token")
+        //TODO: Add info to send to server
+    }
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d(TAG, "From: ${remoteMessage.from}")
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
+        }
+        remoteMessage.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+            sendNotification(it.body ?: "Body")
+        }
+    }
+
+    private fun sendNotification(messageBody: String) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_ONE_SHOT)
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, "1")
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle("Notif")
+            .setContentText(messageBody)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+            .setPriority(2)
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel("1",
+                "Channel human readable title",
+                NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(channel)
+        }
+        notificationManager.notify(0, notificationBuilder.build())
+    }
 }
